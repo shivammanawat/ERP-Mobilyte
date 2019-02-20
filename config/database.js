e8ea1e63f7ef7f314d2f27a1bcc0cd9cdd5a1a0c
shivam manawat <36154183+shivammanawat@users.noreply.github.com>
2019-02-20 17:30:25 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports={
-    database: 'mongodb://localhost:27017/meanProject',
-  
+  //  database: 'mongodb://localhost:27017/meanProject',
+   database:  'mongodb://moberp1:moberp1@ds343985.mlab.com:43985/moberp',
     secret : 'secret your'
 }
